501385a50c73d9592ab151933ff822d7236c0e74
Nahid30 <nahidshuvo30@gmail.com>
2022-05-17 16:32:25 +0600

    solving DB_USER

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_PASS}@cluster0.w63o4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w63o4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next){
