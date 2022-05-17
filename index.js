f302125ad1d4d50ad1d5ca04ee745ba0da9931a7
Nahid30 <nahidshuvo30@gmail.com>
2022-05-18 00:54:16 +0600

    heroku related problem

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w63o4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -23,7 +22,7 @@ function verifyJWT(req, res, next){
   console.log('abc');
   const authHeader = req.headers.authorization;
   if(!authHeader){
-    return res.status(401).send({massage:'Unauthorized'})
+    return res.status(401).send({massage:'Unauthorized'});
   }
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
@@ -63,7 +62,7 @@ async function run() {
       const email = req.params.email;
       const user = await userCollection.findOne({email: email});
       const isAdmin = user.role === 'admin';
-      res.send(isAdmin);
+      res.send(isAdmin); 
     })
 
     app.put('/user/admin/:email', verifyJWT, async (req, res) => {
