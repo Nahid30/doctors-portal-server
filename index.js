3879652eba45fff51c868d8f739278f8b04406a7
Nahid30 <nahidshuvo30@gmail.com>
2022-05-19 06:32:18 +0600

    code cleaning

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 function verifyJWT(req, res, next){
   const authHeader = req.headers.authorization;
   if(!authHeader){
-    return res.status(401).send({massage:'Unauthorized access'});
+    return res.status(401).send({massage:'Unauthorized Access'});
   }
   const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
     if(err){
-      return res.status(403).send({message:'Forbidden access'})
+      return res.status(403).send({message:'Forbidden Access'})
     }
     req.decoded= decoded;
     next();
@@ -45,7 +45,7 @@ async function run() {
 
     app.get('/service', async (req, res) => {
       const query = {};
-      const cursor = serviceCollection.find(query).project({name: 1}) ;
+      const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
@@ -61,9 +61,7 @@ async function run() {
       const email = req.params.email;
       const user = await userCollection.findOne({email: email});
       const isAdmin = user.role === 'admin';
-      // res.send(isAdmin); 
-      res.send({ admin: isAdmin })
-
+      res.send({admin: isAdmin}); 
     })
 
     app.put('/user/admin/:email', verifyJWT, async (req, res) => {
@@ -82,9 +80,8 @@ async function run() {
         res.status(403).send({message:'forbidden'});
       }
       
-      // const token = jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,  { expiresIn: '1h' })
-      
     })
+
 
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
@@ -144,7 +141,6 @@ async function run() {
       const patient = req.query.patient;
       const decodedEmail = req.decoded.email;
       if(patient === decodedEmail){
-        console.log('auth header', authorization);
         const query = { patient: patient };
         const bookings = await bookingCollection.find(query).toArray();
         return res.send(bookings);
